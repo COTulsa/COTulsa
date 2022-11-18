8f9a298957f83d7de81527417a8a2097b7e74c36
COTulsa <113406157+COTulsa@users.noreply.github.com>
2022-11-18 21:33:25 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-function do_KDF_mapReady_Verint_1(event, passed_in_kdf, type, name, map, positionLayer, markerLayer, marker, projection){
+function do_KDF_mapReady(event, passed_in_kdf, type, name, map, positionLayer, markerLayer, marker, projection){
   
 }
 
