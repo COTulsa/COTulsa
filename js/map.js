daa9e6bc7f1a74885dd820b56179f5d3b9a8e152
COTulsa <113406157+COTulsa@users.noreply.github.com>
2022-11-18 21:32:10 +0700

    Create map.js

---
+++ js/map.js
@@ -0,0 +1,9 @@
+function do_KDF_mapReady_Verint_1(event, passed_in_kdf, type, name, map, positionLayer, markerLayer, marker, projection){
+  
+}
+
+
+function do_KDF_mapClicked(event, kdf, type, name, map, positionLayer, markerLayer, marker, lat, lon, plat, plon){
+  
+  
+}
